15e963e8fe6d10d0f8c2601b410c4ecc67690284
Juan Flores <Juan Flores>
2016-09-13 11:12:19 -0600

    functional minifier version

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var fs = require('fs'),
 	path = require('path'),
 	gulp = require('gulp'),
 	sh = require('shelljs'),
+	ngAnnotate  = require('gulp-ng-annotate'),
 	uglify = require('gulp-uglify'),
+	notify = require('gulp-notify'),
 	argv = require('yargs').argv;
 
 gulp.task('testTask', function () {
@@ -55,13 +57,42 @@ gulp.task('copyFiles', function () {
 gulp.task('minify', function () {
 	var destDir = argv.dest;
 	var dirsDestToCompress = [
-		'www/js/**/*.js'
+		'www/js/**/*.provider.js',
+		'www/js/**/*.factory.js',
+		'www/js/**/*.controller.js',
+		'www/js/**/*.service.js'
 	];
 	dirsDestToCompress.forEach(function (relPath, idx) {
 		var path = destDir+'/'+relPath;
-		console.log(path);
-		gulp.src([path,'!'+destDir+'/'+'www/js/app.js'])
+		gulp.src([path,'!'+destDir+'/'+'www/js/app.js','!www/js/**/*.min.js'])
+			.pipe(ngAnnotate())
+			.on('error', notify.onError("Error: <%= error.message %>"))
 			.pipe(uglify())
-			.pipe(gulp.dest(destDir+'/'+'www/js'));
+			.on('error', notify.onError("Error: <%= error.message %>"))
+			.pipe(gulp.dest(destDir+'/'+'www/js/'));
+	});
+});
+
+gulp.task('restoreJS',function(){
+	var basePath = argv.source;
+	var destDir = argv.dest;
+	var directoriesToCopy = [
+		'www'
+	];
+	if (!sh.test('-e', destDir)) {
+		console.log("Directory " + destDir + " does not exists, creating it...");
+		sh.mkdir('-p', destDir);
+	}
+
+	directoriesToCopy.forEach(function (dir, idx) {
+		var commandToExec = "cp -R " + basePath + '/' + dir + ' ' + destDir;
+		sh.exec(commandToExec, function (code, stdout, stderr) {
+			if (code === 0) {
+				console.log("Sucessfully copied " + basePath + '/' + dir + " to " + destDir);
+			} else {
+				console.log("Failed to copy " + basePath + '/' + dir + " to " + destDir);
+				console.log("Error", stderr);
+			}
+		});
 	});
 });
